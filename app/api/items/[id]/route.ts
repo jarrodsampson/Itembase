6c321934af0d7bc0558b70379b2d30521a0e32e4
Jarrod Sampson <>
2023-11-27 22:52:52 -0800

    moving to nextjs 13.5.6, adding all api routes, bulk delete, updated, create, read with limit, offset, and search, single item, update, delete, and search

---
+++ app/api/items/[id]/route.ts
@@ -0,0 +1,111 @@
+import { NextRequest, NextResponse } from "next/server";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export async function GET(
+  req: Request,
+  { params }: { params: { id: string } }
+) {
+  const { id } = params;
+  const itemId = id as string;
+
+  try {
+    const userSearch = await prisma.item.findUnique({
+      where: {
+        id: parseInt(itemId, 10),
+      },
+    });
+    if (userSearch) {
+      return NextResponse.json({ userSearch }, { status: 200 });
+    } else {
+      return NextResponse.json({ error: "Item not found" }, { status: 404 });
+    }
+  } catch (e: unknown) {
+    return NextResponse.json(
+      {
+        error: "An error occurred while processing your request",
+      },
+      {
+        status: 500,
+      }
+    );
+  } finally {
+    await prisma.$disconnect();
+  }
+}
+
+export async function DELETE(
+  req: Request,
+  { params }: { params: { id: string } }
+) {
+  const { id } = params;
+  const itemId = id as string;
+
+  try {
+    const userDelete = await prisma.item.delete({
+      where: {
+        id: parseInt(itemId, 10),
+      },
+    });
+
+    if (userDelete) {
+      return NextResponse.json(
+        { message: "Item successfully deleted" },
+        { status: 200 }
+      );
+    } else {
+      return NextResponse.json({ error: "Item not found" }, { status: 404 });
+    }
+  } catch (e: unknown) {
+    return NextResponse.json(
+      { error: "An error occurred while processing your request" },
+      { status: 500 }
+    );
+  } finally {
+    await prisma.$disconnect();
+  }
+}
+
+export async function PATCH(
+  req: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  const { id } = params;
+  try {
+    const { name, message } = await req.json();
+
+    if (!name) {
+      NextResponse.json(
+        { error: "Invalid request: Missing item name" },
+        { status: 400 }
+      );
+      return;
+    }
+
+    const itemId = id as string;
+
+    const userUpdate = await prisma.item.update({
+      where: {
+        id: parseInt(itemId, 10),
+      },
+      data: {
+        name,
+        message,
+      },
+    });
+
+    if (userUpdate) {
+      return NextResponse.json({ userUpdate }, { status: 200 });
+    } else {
+      return NextResponse.json({ error: "Item not found" }, { status: 404 });
+    }
+  } catch (e: unknown) {
+    return NextResponse.json(
+      { error: "An error occurred while processing your request" },
+      { status: 500 }
+    );
+  } finally {
+    await prisma.$disconnect();
+  }
+}
--- app/api/items/route.ts
+++ app/api/items/route.ts
@@ -1,11 +1,166 @@
 import { NextRequest, NextResponse } from "next/server";
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-export async function GET (
-  req: NextRequest
-) {
+export async function GET(req: NextRequest) {
+  const limit = req.nextUrl.searchParams.get("limit");
+  const offset = req.nextUrl.searchParams.get("offset");
+  const search = req.nextUrl.searchParams.get("search");
 
-  return NextResponse.json({ message: 'Item successfully deleted' });
+  try {
+    let items = [];
+    let total: Number = 0;
+    let where = {
+      name: {
+        contains: `${search}`,
+      },
+    };
+    items = await prisma.item.findMany({
+      skip: Number(offset) || 0,
+      take: Number(limit) || 10,
+      where,
+    });
+    total = await prisma.item.count({
+      where: where,
+    });
+    if (search) {
+    } else {
+      items = await prisma.item.findMany({
+        skip: Number(offset) || 0,
+        take: Number(limit) || 10,
+      });
+      total = await prisma.item.count();
+    }
+
+    return NextResponse.json(
+      { items, total },
+      {
+        status: 200,
+      }
+    );
+  } catch (e: unknown) {
+    return NextResponse.json(
+      {
+        error: "An error occurred while processing your request",
+      },
+      {
+        status: 500,
+      }
+    );
+  } finally {
+    await prisma.$disconnect();
+  }
+}
+
+export async function POST(req: NextRequest) {
+  const { name, message } = await req.json();
+  if (name) {
+    try {
+      const user = await prisma.item.create({
+        data: {
+          name,
+          message,
+        },
+      });
+      return NextResponse.json({ item: user }, { status: 201 });
+    } catch (e: unknown) {
+      if (e instanceof Error) {
+        if (e.message.includes("Unique")) {
+          return NextResponse.json(
+            { error: "Duplicate Item detected" },
+            { status: 400 }
+          );
+        } else {
+          throw e;
+        }
+      } else {
+        return NextResponse.json(
+          { error: "An error occurred while processing your request" },
+          { status: 500 }
+        );
+      }
+    } finally {
+      await prisma.$disconnect();
+    }
+  } else {
+    return NextResponse.json(
+      { error: "Invalid request: Missing item name" },
+      { status: 400 }
+    );
+  }
+}
+
+export async function DELETE(req: NextRequest) {
+  try {
+    const { itemIds } = await req.json();
+    // check for validity first
+    if (!itemIds || !Array.isArray(itemIds) || itemIds.length === 0) {
+      return NextResponse.json({ error: "Invalid item IDs" }, { status: 400 });
+    }
+
+    const deleteResult = await prisma.item.deleteMany({
+      where: {
+        id: {
+          in: itemIds, // Filter by the list of item IDs
+        },
+      },
+    });
+
+    if (deleteResult.count > 0) {
+      return NextResponse.json(
+        { message: "Items Successfully Deleted." },
+        { status: 200 }
+      );
+    } else {
+      return NextResponse.json(
+        { error: "No items found to delete" },
+        { status: 404 }
+      );
+    }
+  } catch (e: unknown) {
+    return NextResponse.json(
+      { error: "An error occurred while processing your request" },
+      { status: 500 }
+    );
+  } finally {
+    await prisma.$disconnect();
+  }
+}
+
+export async function PATCH(req: NextRequest) {
+  try {
+    const { itemIds, message } = await req.json();
+
+    // Use Prisma to delete the items by their IDs
+    const updateUsers = await prisma.item.updateMany({
+      where: {
+        id: {
+          in: itemIds, // Filter by the list of item IDs
+        },
+      },
+      data: {
+        message,
+      },
+    });
+
+    if (updateUsers.count > 0) {
+      return NextResponse.json(
+        { message: "Items Successfully Updated." },
+        { status: 200 }
+      );
+    } else {
+      return NextResponse.json(
+        { error: "No items found to update" },
+        { status: 404 }
+      );
+    }
+  } catch (e: unknown) {
+    return NextResponse.json(
+      { error: "An error occurred while processing your request" },
+      { status: 500 }
+    );
+  } finally {
+    await prisma.$disconnect();
+  }
 }
